fd321a5ac0b15500754becff87376cc6afc858fb
Abbey Fitzgerald <afitzgerald@MacBook-Pro-2.local>
2017-05-11 22:26:44 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+
+function floor(diameter) {
+	var radius = diameter/2;
+	var floorArea = Math.PI * (radius * radius);
+	return floorArea;
+}
+
+function walls(diameter, height) {
+	var radius = diameter/2;
+	var circumfrance = 2 * Math.PI + radius;
+	var wallArea = circumfrance * height;
+	return wallArea;
+}
+
+function materialsNeeded() {
+
+	var d = document.getElementById("across").value;
+	var h = document.getElementById("height").value;
+
+	var carpetNeeded = Math.ceil(floor(d));
+	var paintNeeded = Math.ceil(walls(d, h));
+
+	console.log('Carpet needed is '+ carpetNeeded +' sqft');
+	console.log(paintNeeded);
+
+} // end of function
+
+
